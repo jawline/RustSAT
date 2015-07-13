47b1693c3ee573c66a9d43bc624421a95e1afcfe
Blake Loring <blake_l@parsed.uk>
2015-07-13 22:24:42 +0100

    Add one two and three functions

--- src/clause.rs
+++ src/clause.rs
@@ -9,10 +9,22 @@ impl <'a>Item<'a> {
 	pub fn new(variable: &'a Variable, not: bool) -> Item<'a> {
 		Item{variable: variable, not: not}
 	}
+
+	pub fn clone(&self) -> Item<'a> {
+		Item{variable: self.variable, not: self.not}
+	}
 }
 
 pub type Clause<'a> = (Item<'a>, Item<'a>, Item<'a>);
 
-pub fn clause<'a>(a: Item<'a>, b: Item<'a>, c: Item<'a>) -> Clause<'a> {
+pub fn one<'a>(a: Item<'a>) -> Clause<'a> {
+	(a.clone(), a.clone(), a)
+}
+
+pub fn two<'a>(a: Item<'a>, b: Item<'a>) -> Clause<'a> {
+	(a.clone(), b, a)
+}
+
+pub fn three<'a>(a: Item<'a>, b: Item<'a>, c: Item<'a>) -> Clause<'a> {
 	(a, b, c)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ pub mod variable;
 pub mod clause;
 pub mod cnf;
 
+use variable::Variable;
 use clause::Item;
 
 fn main() {
 	
-	let a = variable::Variable::new("a");
-	let b = variable::Variable::new("b");
-	let c = variable::Variable::new("c");
+	let a = Variable::new("a");
+	let b = Variable::new("b");
+	let c = Variable::new("c");
 
-	let clause_one = clause::clause(Item::new(&a, true), Item::new(&b, false), Item::new(&a, true));
-	let clause_two = clause::clause(Item::new(&b, true), Item::new(&c, false), Item::new(&b, true));
+	let clause_one = clause::two(Item::new(&a, true), Item::new(&b, false));
+	let clause_two = clause::two(Item::new(&b, true), Item::new(&c, false));
 
 	let mut cnf = cnf::CNF::new();
 	cnf.add(clause_one);

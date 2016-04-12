9c150144514eccc191c3383155d25d00e3497c43
Blake Loring <blake_l@parsed.uk>
2016-04-12 11:31:19 +0100

    Building the solver

--- src/cnf.rs
+++ src/cnf.rs
@@ -8,7 +8,7 @@ pub struct CNF<'a> {
 
 impl <'a>CNF<'a> {
 	pub fn new() -> CNF<'a> {
-		CNF{
+		CNF {
 			items: Vec::new()
 		}
 	}
@@ -17,11 +17,47 @@ impl <'a>CNF<'a> {
 		self.items.push(item)
 	}
 
+	pub fn is_satisfied(&self, variables: &[&Variable], allocation: &Vec<bool>) -> bool {
+		true
+	}
+
+	pub fn satisfy_from(&self, i: usize, variables: &[&Variable], allocation: &mut Vec<bool>) -> bool {
+		
+		for j in (i+1)..allocation.len() {
+			allocation[j] = true;
+			
+			if self.is_satisfied(variables, allocation) {
+				return true;
+			}
+
+			allocation[j] = false;
+		}
+
+		false
+	}
+
 	pub fn can_satisfy(&self, variables: &[&Variable]) -> bool {
-		let mut current_allocation = Vec::<bool>::new();
+		
+		let mut current_allocation = Vec::new();
 
 		for var in variables {
 			current_allocation.push(false);
+		}
+
+		if self.is_satisfied(variables, &current_allocation) {
+			return true;
+		}
+
+		for i in 0..current_allocation.len() {
+			current_allocation[i] = true;
+
+			if self.is_satisfied(variables, &current_allocation) {
+				return true;
+			}
+
+			if self.satisfy_from(i, variables, &mut current_allocation) {
+				return true;
+			}
 		}
 
 		false
